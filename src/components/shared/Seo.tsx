66590ed13682e9a97e711b414e01f73cc3895c21
Peterdinis <peterdinis0@pobox.sk>
2023-06-03 14:33:56 +0200

    feat: Adding prisma

---
+++ src/components/shared/Seo.tsx
@@ -0,0 +1,11 @@
+import Head from "next/head";
+
+const Seo: React.FC = () => {
+    return (
+        <Head>
+            <title>Next-Food-Shop</title>
+        </Head>
+    )
+}
+
+export default Seo;
