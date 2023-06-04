218d3cf6f5db6436214246e7e9783aeb3700f41a
Peterdinis <peterdinis0@pobox.sk>
2023-06-04 15:00:57 +0200

    FOO

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,9 +1,10 @@
+import SearchForm from "@/components/foods/SeachForm";
 import { NextPage } from "next";
 
 const SearchPage: NextPage = () => {
     return (
         <>
-        
+            <SearchForm />
         </>
     )
 }
---
+++ src/components/foods/SeachForm.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import { Container } from "@chakra-ui/react";
+
+const SearchForm: React.FC = () => {
+    return (
+        <Container>
+        I AM SEARCH FORM
+        </Container>
+    )
+}
+
+export default SearchForm;
