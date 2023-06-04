c2cbb1e391e562b0243e5943f521513584dd7b51
Peterdinis <peterdinis0@pobox.sk>
2023-06-04 15:28:21 +0200

    foo

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -1,42 +1,62 @@
 "use client"
 
-import { NavbarContainer, NavbarLink, NavbarLinks, NavbarLogo } from "@/styles/Component.styled";
-import { useState } from "react";
-import Link from 'next/link';
+import React, { useState } from 'react';
+import {
+  Box,
+  Flex,
+  Spacer,
+  IconButton,
+  useDisclosure,
+  Collapse,
+  Link,
+} from '@chakra-ui/react';
+import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons';
+import NextLink from 'next/link';
 
-const Navbar: React.FC = () => {
-  const [isOpen, setIsOpen] = useState(false);
+const Navbar = () => {
+  const { isOpen, onToggle } = useDisclosure();
 
-  const toggleNavbar = () => {
-    setIsOpen(!isOpen);
-  };
   return (
-    <NavbarContainer>
-    <Link legacyBehavior href="/">
-      <NavbarLogo>Logo</NavbarLogo>
-    </Link>
-    <button onClick={toggleNavbar}>
-      {isOpen ? 'Close' : 'Menu'}
-    </button>
-    <NavbarLinks isOpen={isOpen}>
-      <NavbarLink>
-        <Link legacyBehavior href="/about">
-          <span>About</span>
-        </Link>
-      </NavbarLink>
-      <NavbarLink>
-        <Link href="/services">
-          <span>Services</span>
-        </Link>
-      </NavbarLink>
-      <NavbarLink>
-        <Link legacyBehavior href="/contact">
-          <span>Contact</span>
-        </Link>
-      </NavbarLink>
-    </NavbarLinks>
-  </NavbarContainer>
-  )
-}
+    <Box bg="gray.100" px={4} py={3}>
+      <Flex maxW="1200px" mx="auto" align="center">
+        <NextLink href="/" passHref>
+          <Link fontSize="xl" fontWeight="bold" color="gray.700">
+            Logo
+          </Link>
+        </NextLink>
+        <Spacer />
+        <IconButton
+          aria-label="Toggle navigation"
+          icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+          display={{ base: 'block', md: 'none' }}
+          onClick={onToggle}
+        />
+        <Collapse in={isOpen} animateOpacity>
+          <Flex
+            mt={{ base: 4, md: 0 }}
+            direction={{ base: 'column', md: 'row' }}
+            alignItems="center"
+            justifyContent="flex-end"
+            flexGrow={1}
+          >
+            <NextLink href="/about" passHref>
+              <Link mr={{ base: 0, md: 4 }} mb={{ base: 2, md: 0 }}>
+                About
+              </Link>
+            </NextLink>
+            <NextLink href="/services" passHref>
+              <Link mr={{ base: 0, md: 4 }} mb={{ base: 2, md: 0 }}>
+                Services
+              </Link>
+            </NextLink>
+            <NextLink href="/contact" passHref>
+              <Link>Contact</Link>
+            </NextLink>
+          </Flex>
+        </Collapse>
+      </Flex>
+    </Box>
+  );
+};
 
 export default Navbar;
