4e7d9941c6177803da5f2a00cdb08a81e99b9812
Peterdinis <peterdinis0@pobox.sk>
2023-06-18 17:49:17 +0200

    feat: Finish basic ui refactor

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -18,17 +18,15 @@ const Hero: React.FC<HeroProps> = ({ imageUrl, title, description, buttonText })
           style={{ backgroundImage: `url(${imageUrl})` }}
         ></div>
       </div>
-      <div className="relative container mx-auto px-4 py-24">
-        <div className="text-center text-white">
-          <h1 className="text-4xl font-bold mb-4">{title}</h1>
-          <p className="text-lg mb-8">{description}</p>
-          <a
-            href="#menu"
-            className="bg-yellow-500 hover:bg-yellow-600 text-white py-2 px-6 rounded-full uppercase tracking-wide"
-          >
-            {buttonText}
-          </a>
-        </div>
+      <div className="flex flex-col justify-center items-center h-full relative">
+        <h1 className="text-6xl font-bold text-white mb-4 text-shadow-md">{title}</h1>
+        <p className="text-lg text-white mb-8">{description}</p>
+        <a
+          href="#menu"
+          className="bg-yellow-500 hover:bg-yellow-600 text-white py-2 px-6 rounded-full uppercase tracking-wide text-shadow-md"
+        >
+          {buttonText}
+        </a>
       </div>
     </section>
   );
