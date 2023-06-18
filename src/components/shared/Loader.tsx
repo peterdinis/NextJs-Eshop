8bea2b3a32c0995fab4c0ae1d7d8020968702ea3
Peterdinis <peterdinis0@pobox.sk>
2023-06-18 16:40:43 +0200

    FOO

--- src/components/shared/Loader.tsx
+++ src/components/shared/Loader.tsx
@@ -1,24 +1,9 @@
-import { Container, Stack, Box, Spinner } from "@chakra-ui/react";
-
 const Loader: React.FC = () => {
   return (
-    <Container maxW={"3xl"}>
-      <Stack
-        as={Box}
-        textAlign={"center"}
-        spacing={{ base: 8, md: 14 }}
-        py={{ base: 20, md: 36 }}
-      >
-        <Spinner
-          thickness="4px"
-          speed="0.65s"
-          emptyColor="gray.200"
-          color="blue.500"
-          size="xl"
-        />
-      </Stack>
-    </Container>
-  );
-};
+    <>
+    ffff
+    </>
+  )
+}
 
-export default Loader;
+export default Loader
