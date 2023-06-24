e41b7f6ef614dc188f3fd94add5f2ff1f4f9f675
Peterdinis <peterdinis0@pobox.sk>
2023-06-24 09:49:59 +0200

    feat: Updating our auth forms

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import Link from "next/link";
 import Header from "../shared/Header";
 import Navbar from "../shared/Navbar";
 
@@ -8,6 +9,60 @@ const LoginForm: React.FC = () =>{
         <>
           <Navbar />
           <Header name="Login" />
+          <form>
+                <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col">
+                    <div className="mb-4">
+                        <div className="mb-2">
+                            <label
+                                className="block text-grey-darker text-sm font-bold mb-2"
+                                htmlFor="password"
+                            >
+                                Email
+                            </label>
+                            <input
+                                className="passwordInput shadow appearance-none border border-red rounded w-full py-2 px-3 text-grey-darker mb-3"
+                                id="Email"
+                                type="text"
+                                autoFocus
+                                placeholder="Email"
+                            />
+                        </div>
+
+                        <div className="mb-2">
+                            <label
+                                className="block text-grey-darker text-sm font-bold mb-2"
+                                htmlFor="password"
+                            >
+                                Heslo
+                            </label>
+                            <input
+                                className="passwordInput shadow appearance-none border border-red rounded w-full py-2 px-3 text-grey-darker mb-3"
+                                id="Heslo"
+                                type="password"
+                                autoFocus
+                                autoComplete="current-password"
+                                placeholder="********************************************"
+                            />
+                        </div>
+                        <div>
+                            <button
+                                className="mt-4 bg-red-700 rounded-lg p-2 text-white"
+                                type="submit"
+                            >
+                                Prihlásenie
+                            </button>
+                            <div>
+                                <Link
+                                    className="mt-4 inline-block align-baseline font-bold text-2xl text-blue hover:text-blue-darker"
+                                    href="/admin/register"
+                                >
+                                    Registrácia
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </form>
         </>
     )
 }
--- src/components/hero/ServicesImages.tsx
+++ src/components/hero/ServicesImages.tsx
@@ -1,15 +1,19 @@
+import Image from "next/image";
+import FirstImage from "../images/firstImage.jpg";
+
+/* TODO: Add img to file image later */
+
 const ServicesImages: React.FC = () => {
     return (
         <div className="lg:w-7/12 lg:mt-0 mt-8">
-        <div className="w-full h-full bg-red-200">
-            <img src="https://i.ibb.co/cbyDY74/pexels-max-vakhtbovych-6782351-1-1.png" alt="apartment design" className="w-full sm:block hidden" />
-            <img src="https://i.ibb.co/ZVPGjGJ/pexels-max-vakhtbovych-6782351-1.png" alt="apartment design" className="sm:hidden block w-full" />
+            <div className="w-full h-full bg-red-200">
+                <Image src={FirstImage} alt="Image" className="w-full sm:block hidden" />
+            </div>
+            <div className="grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 lg:gap-8 gap-6 lg:mt-8 md:mt-6 mt-4">
+                <img src="https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aGVhbHRoeSUyMGZvb2R8ZW58MHx8MHx8fDA%3D&w=1000&q=80" className="w-full" alt="kitchen" />
+                <img src="https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aGVhbHRoeSUyMGZvb2R8ZW58MHx8MHx8fDA%3D&w=1000&q=80" className="w-full" alt="sitting room" />
+            </div>
         </div>
-        <div className="grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 lg:gap-8 gap-6 lg:mt-8 md:mt-6 mt-4">
-            <img src="https://i.ibb.co/4Jrp5TB/pexels-max-vakhtbovych-6782370-1.png" className="w-full" alt="kitchen" />
-            <img src="https://i.ibb.co/0Jv3FSy/pexels-max-vakhtbovych-6436799-1-1.png" className="w-full" alt="sitting room" />
-        </div>
-    </div>
     )
 }
 
