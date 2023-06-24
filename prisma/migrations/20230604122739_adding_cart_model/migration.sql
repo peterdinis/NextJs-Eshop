5cc711f7026de2f9ade0f177108fa801f90e8e53
Peterdinis <peterdinis0@pobox.sk>
2023-06-24 09:03:13 +0200

    feat: Replace sqlite for postgresql wip

--- prisma/migrations/20230604122739_adding_cart_model/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Card" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "productId" INTEGER NOT NULL,
-    "amount" INTEGER NOT NULL DEFAULT 1,
-    "totalPrice" DECIMAL NOT NULL,
-    CONSTRAINT "Card_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
-);
