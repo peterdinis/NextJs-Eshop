5b9bf232197c0462242f3aaca4292b67d3c7ccd3
Peterdinis <peterdinis0@pobox.sk>
2023-06-24 13:13:26 +0200

    FOO

--- src/app/foods/page.tsx
+++ src/app/foods/page.tsx
@@ -1,11 +1,9 @@
 import FoodLists from "@/components/foods/FoodLists";
-import Navbar from "@/components/shared/Navbar";
 import { NextPage } from "next";
 
 const FoodsPage: NextPage = () => {
     return (
       <>
-        <Navbar />
         <FoodLists />
       </>
     )
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import '../styles/main.css'
 import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'Food Eshop',
