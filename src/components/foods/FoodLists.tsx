70a2ab8b1dbb71d99d0627281e9a47a7f21111a7
Peterdinis <peterdinis0@pobox.sk>
2023-06-25 10:41:53 +0200

    feat: Adding animation for foodList component

--- src/components/foods/FoodLists.tsx
+++ src/components/foods/FoodLists.tsx
@@ -1,72 +1,112 @@
-import ScrollToTop from "@/hooks/useScroll"
-import Header from "../shared/Header"
-import Link from "next/link"
+"use client"
+
+import { motion } from 'framer-motion';
+import ScrollToTop from '@/hooks/useScroll';
+import Header from '../shared/Header';
+import Link from 'next/link';
 
 const FoodLists: React.FC = () => {
-    return (
-        <>
-            <Header name="All Foods" />
-            <div className="grid grid-cols-4 gap-4 mt-4">
-                <div
-                    className="bg-white p-4 shadow-md rounded-md"
-                >
-                    <img
-                        src="https://picsum.photos/200/300"
-                        alt="TOTO"
-                        className="w-full h-40 object-cover rounded-md mb-4"
-                    />
-                    <h3 className="text-lg font-medium mb-2">z</h3>
-                    <p className="text-gray-600">r</p>
-                    <div className="mt-4 flex justify-center align-top bg-blue-400 text-white rounded-lg">
-                        <Link href="/">DETAIL</Link>
-                    </div>
-                </div>
-                <div
-                    className="bg-white p-4 shadow-md rounded-md"
-                >
-                    <img
-                        src="https://picsum.photos/200/300"
-                        alt="TOTO"
-                        className="w-full h-40 object-cover rounded-md mb-4"
-                    />
-                    <h3 className="text-lg font-medium mb-2">z</h3>
-                    <p className="text-gray-600">r</p>
-                    <div className="mt-4 flex justify-center align-top bg-blue-400 text-white rounded-lg">
-                        <Link href="/">DETAIL</Link>
-                    </div>
-                </div>
-                <div
-                    className="bg-white p-4 shadow-md rounded-md"
-                >
-                    <img
-                        src="https://picsum.photos/200/300"
-                        alt="TOTO"
-                        className="w-full h-40 object-cover rounded-md mb-4"
-                    />
-                    <h3 className="text-lg font-medium mb-2">z</h3>
-                    <p className="text-gray-600">r</p>
-                    <div className="mt-4 flex justify-center align-top bg-blue-400 text-white rounded-lg">
-                        <Link href="/">DETAIL</Link>
-                    </div>
-                </div>
-                <div
-                    className="bg-white p-4 shadow-md rounded-md"
-                >
-                    <img
-                        src="https://picsum.photos/200/300"
-                        alt="TOTO"
-                        className="w-full h-40 object-cover rounded-md mb-4"
-                    />
-                    <h3 className="text-lg font-medium mb-2">z</h3>
-                    <p className="text-gray-600">r</p>
-                    <div className="mt-4 flex justify-center align-top bg-blue-400 text-white rounded-lg">
-                        <Link href="/">DETAIL</Link>
-                    </div>
-                </div>
-            </div>
-            <ScrollToTop />
-        </>
-    )
-}
+  const itemVariants = {
+    hidden: { opacity: 0, y: 50 },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        duration: 0.5,
+      },
+    },
+  };
 
-export default FoodLists
+  return (
+    <>
+      <Header name="All Foods" />
+      <div className="grid grid-cols-4 gap-4 mt-4">
+        <motion.div
+          className="bg-white p-4 shadow-md rounded-md"
+          variants={itemVariants}
+          initial="hidden"
+          animate="visible"
+        >
+          <motion.img
+            src="https://picsum.photos/200/300"
+            alt="TOTO"
+            className="w-full h-40 object-cover rounded-md mb-4"
+          />
+          <motion.h3 className="text-lg font-medium mb-2">z</motion.h3>
+          <motion.p className="text-gray-600">r</motion.p>
+          <motion.div
+            className="mt-4 flex justify-center align-top bg-blue-400 text-white rounded-lg"
+            whileHover={{ scale: 1.05 }}
+            whileTap={{ scale: 0.95 }}
+          >
+            <Link href="/">DETAIL</Link>
+          </motion.div>
+        </motion.div>
+        <motion.div
+          className="bg-white p-4 shadow-md rounded-md"
+          variants={itemVariants}
+          initial="hidden"
+          animate="visible"
+        >
+          <motion.img
+            src="https://picsum.photos/200/300"
+            alt="TOTO"
+            className="w-full h-40 object-cover rounded-md mb-4"
+          />
+          <motion.h3 className="text-lg font-medium mb-2">z</motion.h3>
+          <motion.p className="text-gray-600">r</motion.p>
+          <motion.div
+            className="mt-4 flex justify-center align-top bg-blue-400 text-white rounded-lg"
+            whileHover={{ scale: 1.05 }}
+            whileTap={{ scale: 0.95 }}
+          >
+            <Link href="/">DETAIL</Link>
+          </motion.div>
+        </motion.div><motion.div
+          className="bg-white p-4 shadow-md rounded-md"
+          variants={itemVariants}
+          initial="hidden"
+          animate="visible"
+        >
+          <motion.img
+            src="https://picsum.photos/200/300"
+            alt="TOTO"
+            className="w-full h-40 object-cover rounded-md mb-4"
+          />
+          <motion.h3 className="text-lg font-medium mb-2">z</motion.h3>
+          <motion.p className="text-gray-600">r</motion.p>
+          <motion.div
+            className="mt-4 flex justify-center align-top bg-blue-400 text-white rounded-lg"
+            whileHover={{ scale: 1.05 }}
+            whileTap={{ scale: 0.95 }}
+          >
+            <Link href="/">DETAIL</Link>
+          </motion.div>
+        </motion.div><motion.div
+          className="bg-white p-4 shadow-md rounded-md"
+          variants={itemVariants}
+          initial="hidden"
+          animate="visible"
+        >
+          <motion.img
+            src="https://picsum.photos/200/300"
+            alt="TOTO"
+            className="w-full h-40 object-cover rounded-md mb-4"
+          />
+          <motion.h3 className="text-lg font-medium mb-2">z</motion.h3>
+          <motion.p className="text-gray-600">r</motion.p>
+          <motion.div
+            className="mt-4 flex justify-center align-top bg-blue-400 text-white rounded-lg"
+            whileHover={{ scale: 1.05 }}
+            whileTap={{ scale: 0.95 }}
+          >
+            <Link href="/">DETAIL</Link>
+          </motion.div>
+        </motion.div>
+      </div>
+      <ScrollToTop />
+    </>
+  );
+};
+
+export default FoodLists;
--- src/components/shared/Loader.tsx
+++ src/components/shared/Loader.tsx
@@ -1,9 +1,37 @@
-const Loader: React.FC = () => {
-  return (
-    <>
-    ffff
-    </>
-  )
+import { motion, Variants } from 'framer-motion';
+
+interface ILoadingProps {
+  message: string;
 }
 
-export default Loader
+const loadingVariants: Variants = {
+  animation: {
+    scale: [1, 0.5, 1],
+    transition: {
+      duration: 0.5,
+      repeat: Infinity,
+      repeatType: 'reverse',
+      ease: 'easeInOut',
+    },
+  },
+};
+
+const Loader: React.FC<ILoadingProps> = ({message}: ILoadingProps) => {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <motion.div
+        style={{
+          width: 50,
+          height: 50,
+          borderRadius: '50%',
+          background: '#007BFF',
+        }}
+        variants={loadingVariants}
+        animate="animation"
+      />
+      {message}
+    </div>
+  );
+};
+
+export default Loader;
