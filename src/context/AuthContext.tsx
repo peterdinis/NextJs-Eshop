afa59c35bc1772505c6079702285ce50b248a274
Peterdinis <peterdinis0@pobox.sk>
2023-07-01 13:57:37 +0200

    feat: Writing firebase context wip

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,89 @@
+import React, { createContext, useContext, useEffect, useState } from 'react';
+import { auth } from '../lib/firebase';
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  sendPasswordResetEmail,
+  onAuthStateChanged,
+  signInWithPopup,
+  GoogleAuthProvider,
+  signOut,
+  confirmPasswordReset,
+} from 'firebase/auth';
+
+interface AuthContextProps {
+  currentUser: any;
+  signInWithGoogle: () => Promise<void>;
+  login: (email: string, password: string) => Promise<void>;
+  register: (email: string, password: string) => Promise<void>;
+  logout: () => Promise<void>;
+  forgotPassword: (email: string) => Promise<void>;
+  resetPassword: (oobCode: string, newPassword: string) => Promise<void>;
+}
+
+const AuthContext = createContext<AuthContextProps>({
+  currentUser: null,
+  signInWithGoogle: async () => {},
+  login: async () => {},
+  register: async () => {},
+  logout: async () => {},
+  forgotPassword: async () => {},
+  resetPassword: async () => {},
+});
+
+export const useAuth = () => useContext(AuthContext);
+
+export default function AuthContextProvider({ children }: { children: React.ReactNode }) {
+  const [currentUser, setCurrentUser] = useState<any>(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setCurrentUser(user ? user : null);
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  useEffect(() => {
+    console.log('The user is', currentUser);
+  }, [currentUser]);
+
+  function login(email: string, password: string) {
+    return signInWithEmailAndPassword(auth, email, password);
+  }
+
+  function register(email: string, password: string) {
+    return createUserWithEmailAndPassword(auth, email, password);
+  }
+
+  function forgotPassword(email: string) {
+    return sendPasswordResetEmail(auth, email, {
+      url: `http://localhost:3000/login`,
+    });
+  }
+
+  function resetPassword(oobCode: string, newPassword: string) {
+    return confirmPasswordReset(auth, oobCode, newPassword);
+  }
+
+  function logout() {
+    return signOut(auth);
+  }
+
+  function signInWithGoogle() {
+    const provider = new GoogleAuthProvider();
+    return signInWithPopup(auth, provider);
+  }
+
+  const value: AuthContextProps = {
+    currentUser,
+    signInWithGoogle,
+    login,
+    register,
+    logout,
+    forgotPassword,
+    resetPassword,
+  };
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+}
